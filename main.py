1790f6c66ecd36bee68b47beea49221154712a3b
dalejandroramirez <dalejandro.ramirez@udea.edu.co>
2023-11-30 20:46:28 -0500

    feature: EDA preliminar de steam games

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+def PlayTimeGenre(genero: str):
+  "Año con más horas jugadas para dicho género."
+  pass
+
+
+def UserForGenre( genero : str ):
+  """"
+  el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+  """
+
+
+def UsersNotRecommend( año : int ): 
+  """Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
+  Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+  """
+  pass
+
+def sentiment_analysis( año : int ): 
+  """Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.
+  """
+  pass
+
