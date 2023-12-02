871bcbc34853dfe4ec14fffa7b0b95cb7bc5c677
dalejandroramirez <dalejandro.ramirez@udea.edu.co>
2023-12-02 18:40:04 -0500

    feature: Documentación del segundo endpoint

--- main.py
+++ main.py
@@ -42,39 +42,6 @@ def play_time_genres(genero: str):
 
 
 @app.get('/endpoint2/{genre}')
-def user_for_genre(genre :str):
-  
-  table_2 = pd.read_csv('03_Endpoints/entpoint_2.csv')
-
-  ## Filtro la tabla para quedarme unicamente con registros que contengan el genero deseado
-  genre_filter = table_2[table_2['genres'].str.contains(genre)]
-  
-  ## usuario con mayor tiempo de juego acumulado
-  df_sum_play_time =  genre_filter[['user_id','playtime_forever']].groupby(['user_id']).sum()
-  
-  # usuario con mas horas jugadas en total
-  if len(df_sum_play_time) > 0:
-    user_max_time = df_sum_play_time.idxmax().iloc[0]
-  else:
-    return ('No existe usuario que haya jugado este juego')
-  
-  ## Filtraremos solo por los registros del usuario con mayor tiempo de juego
-  ## nos quedamos con el año de lanzamiento y el tiempo de juego
-  mask = genre_filter['user_id'] == user_max_time
-  user_filter = genre_filter[mask][['release_year','playtime_forever']]
-  
-  # Sumamos las horas jugadas del usuario por año
-  user_filter_sum = user_filter.groupby('release_year').sum().reset_index()
-  
-  # ## Calculamos la suma acumulada
-  user_filter_sum['total_horas'] = user_filter_sum['playtime_forever'].cumsum()
-  
-  target = user_filter_sum.rename(columns={'release_year':'year'})[['year', 'total_horas']].to_dict(orient='records')
-  return {f"Usuario con más horas jugadas para Género {genre}" : user_max_time, "Horas jugadas": target}
-
-
-
-@app.get('/endpoint2v1/{genre}')
 def user_for_genre(genre: str):
     table_2 = pd.read_csv('03_Endpoints/entpoint_2.csv')
 
